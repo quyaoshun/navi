669f4345e5eb6a2dc9dfbc3698e77a439c034224
beforeload <fe.daniel91@gmail.com>
2013-01-15 00:49:58 +0800

    use Schema

--- models/blog.js
+++ models/blog.js
@@ -1,2 +1,19 @@
-var mongoose = require('mongoose')
+var mongoose = require('mongoose'),
+	Comments = new Schema({
+		title: String,
+		body: String,
+		data: Date
+	}),
+	BlogPost = new Schema({
+		author: ObjectId,
+		title: String,
+		body: String,
+		date: Date,
+		comments: [Comments],
+		meta: {
+			votes: Number,
+			favs: Number
+		}
+	})
 
+mongoose.model('BlogPost',BlogPost)
