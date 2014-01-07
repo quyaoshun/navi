7e3f75c10a92f4de408584c93aa1e4de55cc62ae
beforeload <fe.daniel91@gmail.com>
2014-01-07 21:26:24 +0800

    wait to design pages

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,27 @@
-exports.index = function(req, res, next){
-  res.render('index', {
-      title: 'Reading List'
-  })
+module.exports = {
+    index: function(req, res, next) {
+        res.render('index', {
+            title: 'Reading List',
+            layout: 'layout'
+        })
+    },
+    signup: function(req, res, next) {
+
+    },
+    login: function(req, res, next) {},
+    logout: function(req, res, next) {},
+    checkLogin: function(req, res, next) {
+        if (req.session.user) {
+            req.flash('message', '未登录')
+            return res.redirect('/login')
+        }
+        next()
+    },
+    checkNotLogin: function(req, res, next) {
+        if (!req.session.user) {
+            res.flash('message', '已登录')
+            return res.redirect('/')
+        }
+        next()
+    }
 }
