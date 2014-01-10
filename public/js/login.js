8acfb7ba10794d1337eef9a652e35ddf54461418
beforeload <fe.daniel91@gmail.com>
2014-01-10 11:33:00 +0800

    add login test

--- public/js/login.js
+++ public/js/login.js
@@ -1,21 +1,48 @@
-$(function(){
+$(function() {
     var DS = {
         login: $('#login'),
         loginInput: $('#login input'),
         inputEmail: $('#inputEmail'),
         inputPassword: $('inputPassword'),
         btnLogin: $('.d-btn-login')
-    }
-    
-    DS.loginInput.on('focus', function(){
+    },
+    regEmail = /^(\w)+(\.\w+)*@(\w)+((\.\w+)+)$/
+
+    DS.loginInput.on('focus', function() {
         $(this).css({
             'opacity': 0.9,
             '-moz-opacity': 0.9
         })
-    }).on('blur', function(){
+    }).on('blur', function() {
         $(this).css({
             'opacity': 0.7,
             '-moz-opacity': 0.7
         })
     })
+    DS.inputEmail.on('blur', function(e) {
+        e.preventDefault()
+        var _this = this
+        if (inputCheck($(_this), regEmail)) {
+            alert(1)
+        } else {
+            alert(2)
+        }
+    })
+
+    DS.btnLogin.on('click', function(e){
+        e.preventDefault()
+        if(inputCheck(DS.inputEmail, regEmail)){
+            alert(3)
+        }
+    })
+
+    function inputCheck(input, reg) {
+        var formControl = input.parents('div.form-group')
+        if(reg.test(input.val())) {
+            formControl.removeClass('has-error').addClass('has-success')
+            return true
+        }
+        formControl.addClass('has-error').removeClass('has-success')
+        return false
+    }
 })
