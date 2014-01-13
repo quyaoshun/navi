06f639a67ba29b4ea899c37e74e391ddb9a6670e
Dong Liu <fe.daniel91@gmail.com>
2014-01-14 02:02:43 +0800

    Update index.js

--- models/index.js
+++ models/index.js
@@ -4,10 +4,12 @@ var mongoose = require('mongoose'),
 mongoose.connect(config.db, function(err){
     if (err) {
         console.error('connect to %s error: ', config.db, err.message)
+        
+        process.exit(1)
     }
-    
-    /* models */
-    require('./user')
-    
-    exports.User = mongoose.model('User')
 })
+
+
+require('./user')
+
+exports.User = mongoose.model('User')
