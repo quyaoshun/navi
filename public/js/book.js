60907596e4790ed1ba46959efa25d2573480d39d
Dong Liu <fe.daniel91@gmail.com>
2014-02-06 14:14:29 +0800

    Update book.js

--- public/js/book.js
+++ public/js/book.js
@@ -1,12 +1,12 @@
 $(function() {
     DS = {
-        bookTitle: $('#booktitle'),
-        bookDesc: $('#bookdesc'),
-        bookAuthor: $('#bookauthor'),
-        bookPress: $('#bookpress'),
-        bookReleaseDate: $('#bookreleasedate'),
-        bookSubmit: $('#booksubmit'),
-
+        bookTitle: $('#book-title'),
+        bookDesc: $('#book-desc'),
+        bookAuthor: $('#book-author'),
+        bookPress: $('#book-press'),
+        bookReleaseDate: $('#book-releasedate'),
+        bookSubmit: $('#book-submit'),
+        bookTable: $('#book-table')
     }
     DS.bookSubmit.on('click', function(e) {
         e.preventDefault()
@@ -25,6 +25,32 @@ $(function() {
                 if (data.code === 200) {
                     $('#addBook').modal('toggle')
                 }
+            }
+        })
+    })
+    
+    DS.bookTable.delegate('.book-remove', function(){
+        var bookid = $(this).data("bookid")
+        $.ajax({
+            url: '/book/removebook',
+            type: 'post',
+            data:{
+                bookid: bookid
+            },
+            success: function(data){
+                alert(data)
+            }
+        })
+    }).delegate('.book-update', function(){
+        var bookid = $(this).data("bookid")
+        $.ajax({
+            url: 'book/updatebook',
+            type: 'post',
+            data: {
+                bookid: bookid
+            },
+            success: function(data){
+                alert(data)
             }
         })
     })
